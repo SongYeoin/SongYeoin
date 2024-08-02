a5ba967f5177c53809812099130b04c915bad5e0
RACHEL-2929 <sts06154@naver.com>
2024-08-02 13:43:15 +0900

    change files position

--- src/main/java/com/syi/project/repository/ChatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.syi.project.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.syi.project.model.MessageVO;
-
-public interface ChatRepository extends MongoRepository<MessageVO, String>{
-
-}
